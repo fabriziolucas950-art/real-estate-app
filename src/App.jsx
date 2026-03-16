3989bd29094ecd06ef460851ee4359f1749a8cd5
fabriziolucas950-art <fabriziolucas950@gmail.com>
2026-03-16 18:10:29 -0300

    Refined Sync Part 2: Main Logic & Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { motion, AnimatePresence } from 'framer-motion';
 import Navbar from './components/shared/Navbar';
 import Footer from './components/shared/Footer';
 import Home from './pages/Home';
 import PropertyDetail from './pages/PropertyDetail';
 import AdminDashboard from './pages/AdminDashboard';
-import SEOManager from './components/shared/SEOManager';
-import './App.css';
+import './index.css';
 
 function App() {
   return (
     <Router>
       <div className="app-container">
-        <SEOManager />
         <Navbar />
-        <main className="main-content">
-          <AnimatePresence mode="wait">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/property/:id" element={<PropertyDetail />} />
-              <Route path="/admin/*" element={<AdminDashboard />} />
-            </Routes>
-          </AnimatePresence>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/property/:id" element={<PropertyDetail />} />
+            <Route path="/admin/*" element={<AdminDashboard />} />
+          </Routes>
         </main>
         <Footer />
       </div>
